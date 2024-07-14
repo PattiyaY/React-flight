a008b490c6d29fbbedea549c6c360f4817cdd16c
PattiyaY <pattiya.2547@icloud.com>
2024-07-14 22:01:36 +0700

    connected to MongoDB

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,9 @@ export default function Home() {
   const [flights, setFlight] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(
+      "https://api.flightapi.io/onewaytrip/6690c4a1433021f10ef8794b/HEL/OUL/2024-07-20/1/0/0/Economy/USD"
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -17,9 +19,9 @@ export default function Home() {
     <main>
       {/* <h1>Consuming REST api tutorial</h1> */}
       <h1>Flight Data</h1>
-      {flights.map((flight) => (
+      {/* {flights.map((flight) => (
         <p key={flight.id}>{flight.title}</p>
-      ))}
+      ))} */}
     </main>
   );
 }
